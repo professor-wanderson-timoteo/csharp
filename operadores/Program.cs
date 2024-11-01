85a3f6a1a4f3bf3ea87dbde3864d338154914dc6
Wanderson Timóteo <wanderson_timoteo@outlook.com>
2024-11-01 17:15:07 -0400

    Operadores Aritméticos

--- operadores/Program.cs
+++ operadores/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Operadores Aritméticos
+double resultado = 12 + 8 + 12 / 3;
+Console.WriteLine(resultado);
+
+double resultado = (12 + 8 + 12) / 3;
+Console.WriteLine(resultado);
+
+double resultado = (2 + 8) * 2 / 4;
+Console.WriteLine(resultado);
