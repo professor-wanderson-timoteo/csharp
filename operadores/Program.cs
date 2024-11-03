5de0748130a51d2400899050865d4de2212685f2
Wanderson Timóteo <wanderson_timoteo@outlook.com>
2024-11-02 21:47:37 -0400

    add exemplo de precedência de operadores lógicos

--- operadores/Program.cs
+++ operadores/Program.cs
@@ -9,3 +9,15 @@ Console.WriteLine(resultado2);
 
 double resultado3 = (2 + 8) * 2 / 4;
 Console.WriteLine(resultado3);
+
+bool aa = false; 
+bool bb = false; 
+bool cc = false;
+bool resultado4 = !aa || bb && cc;
+Console.WriteLine(resultado4);
+
+bool a = true; 
+bool b = false; 
+bool c = false;
+bool resultado5 = !a || (b && c);
+Console.WriteLine(resultado5);
