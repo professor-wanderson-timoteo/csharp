eb9b901edc73c4a9b98709fb37d31c0c13c0267d
Wanderson Timóteo <wanderson_timoteo@outlook.com>
2024-11-06 08:18:46 -0400

    add concatenação e interpolação de strings

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Contatenação de strings usando o operador +
+string nome1 = "Wanderson"; 
+string saudacao1 = "Olá, " + nome1 + "! Bem-vindo."; 
+Console.WriteLine(saudacao1);
+
+// Interpolação de strings usando $
+string nome2 = "Jeane"; 
+string saudacao2 = $"Olá, {nome2}! Bem-vindo."; 
+Console.WriteLine(saudacao2);
+
