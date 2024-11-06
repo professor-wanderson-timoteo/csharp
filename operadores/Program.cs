05630211c9fc821ab0d9cf0ad047b85eb7a5e865
Wanderson Timóteo <wanderson_timoteo@outlook.com>
2024-11-06 07:56:31 -0400

    add operadores logicos e precedencia

--- operadores/Program.cs
+++ operadores/Program.cs
@@ -1,24 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Operadores:
 // Operadores Aritméticos
-double resultado1 = 12 + 8 + 12 / 3;
+double resultado1 = 10 + 8 + 12 / 3;
 Console.WriteLine(resultado1);
 
-double resultado2 = (12 + 8 + 12) / 3;
+double resultado2 = (10 + 8 + 12) / 3;
 Console.WriteLine(resultado2);
 
 double resultado3 = (2 + 8) * 2 / 4;
 Console.WriteLine(resultado3);
 
-bool aa = false; 
-bool bb = false; 
-bool cc = false;
-bool resultado4 = !aa || bb && cc;
+// Operadores de Lógicos
+// Operador AND(&&)
+bool a1 = true; 
+bool b1 = false; 
+Console.WriteLine(a1 && b1);
+
+// Operador OR(||)
+bool a2 = true; 
+bool b2 = false; 
+Console.WriteLine(a2 || b2);
+
+// Operadaor NOT(!)
+bool a3 = true; 
+Console.WriteLine( !a3 );
+
+//Operador XOR(^)
+bool a4 = true; 
+bool b4 = false; 
+Console.WriteLine(a4 ^ b4);
+
+// Precedência de operadores
+bool a5 = false; 
+bool b5 = false; 
+bool c5 = false;
+bool resultado4 = !a5 || b5 && c5; // ! tem maior precedência
 Console.WriteLine(resultado4);
 
 bool a = true; 
 bool b = false; 
 bool c = false;
-bool resultado5 = !a || (b && c);
+bool resultado5 = !a || (b && c); // Parênteses tem maior precedência
 Console.WriteLine(resultado5);
 
