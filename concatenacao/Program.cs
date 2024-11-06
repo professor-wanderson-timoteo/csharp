a55745e726891529f874c332de4046167c169edc
Wanderson Timóteo <wanderson_timoteo@outlook.com>
2024-11-06 08:19:50 -0400

    add operador de tipos e casting string para numeros

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -8,3 +8,18 @@ string nome2 = "Jeane";
 string saudacao2 = $"Olá, {nome2}! Bem-vindo."; 
 Console.WriteLine(saudacao2);
 
+// Operador GetType
+int numero1 = 5;
+Console.WriteLine("O tipo da variável é: " + numero1.GetType()); // Verifica o tipo da variável
+
+// Palavra-chave typeof
+Console.WriteLine("Variáveis 'int' é do tipo: " + typeof(int)); // verifica o tipo de uma classe ou estrutura
+
+// Cast/Casting (Conversão de um número do tipo string para int, float ou double usando Parse )
+string valorTexto = "100";
+Console.WriteLine("String: " + valorTexto);
+Console.WriteLine("A variável valorTexto é do tipo: " + valorTexto.GetType());
+
+int numero2 = int.Parse(valorTexto);
+Console.WriteLine("int: " + numero2);
+Console.WriteLine("A variável numeroParse é do tipo: " + numero2.GetType());
